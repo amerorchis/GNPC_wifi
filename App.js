dc1a13310ed4cb60ea77e67e1bfd4611739551d8
amerorchis <andrew.p.smith95@gmail.com>
2023-06-01 12:57:50 -0600

    Add more logging

--- App.js
+++ App.js
@@ -22,8 +22,9 @@ app.use(express.static("public"))
 
 // GET '/' Endpoint
 app.get(["/", "/apgar", "/depot", "/stmary"], (req, res) => {
+  console.log("This is a console log!");
+  console.log(req.get("referer"));
   res.sendFile(path.join(__dirname, "./public", "index.html"));
-  console.log(req.get("referer"))
 })
 
 // GET error page
